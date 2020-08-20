7e4e81cdd15daaa87bdd9a3b6506be47ba6b0970
JoelSancho <sancho_joel@yahoo.com>
2020-08-20 08:15:05 -0400

    Add row button adds rows

---
+++ main.js
@@ -0,0 +1,60 @@
+// `getElementById()`
+// `addEventListener()`
+// `getElementsByTagName()`
+// `createElement()`
+// `appendChild()`
+// `event.target`
+// `node.children`
+// `Array.from()`
+// `mousedown`
+// `mouseover`
+// `mouseup`
+
+// track the number of row and columns
+let rows = 0;
+let columns = 0;
+
+const table = document.getElementById('my-table');
+
+// target the add-row-button
+const addRowButton = document.getElementById('add-row')
+
+// adding a row:
+// keep track of current # rows
+// keep track of current # columns
+// add a row based on columns
+// increase row count
+
+// adding a row:
+const addRow = () => {
+  const newRow = document.createElement("tr");
+  // add a row based on columns
+  if (columns === 0) {
+    // if no columns, add 1 and increase column count
+    const newCell = document.createElement("td");
+    newRow.appendChild(newCell);
+    columns = 1;
+  } else {
+    // else keep adding a cell for every number of columns
+    for (let i = 0; i < columns; i++) {
+      const newCell = document.createElement("td");
+      newRow.appendChild(newCell);
+    }
+  }
+  //   add new row to the table on page
+  table.appendChild(newRow);
+  // increase row count
+  rows++;
+};
+
+// add an eventlistener:
+addRowButton.addEventListener("click", (event) => {
+  console.log("clicked add row");
+
+// when clicked:
+// add one row to the table
+addRow();
+});
+
+  
+
